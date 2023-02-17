f681b4d685fdb4296e87fed5126c6c8f3637a950
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-02-17 12:44:01 +0200

    changed websoket library to websockets

--- huobi-fetcher.py
+++ huobi-fetcher.py
@@ -1,13 +1,12 @@
 import json
 import requests
-import threading
-import gzip
-import websocket
+import websockets
 import time
+import asyncio
+import gzip
 
 # get all cryptocoin symbols
 currency_url = 'https://api.huobi.pro/v1/settings/common/market-symbols'
-lock = threading.Lock()
 curr_response = requests.get(currency_url)
 resp = curr_response.json()
 size_symbols = dict()
@@ -17,155 +16,111 @@ for i in range(len(resp['data'])):
         size_symbols[resp['data'][i]['symbol']] = (resp['data'][i]['bc']).upper() + \
                                                   '-' + (resp['data'][i]['qc']).upper()
         symbol_dict[resp['data'][i]['bc']] = resp['data'][i]['qc']
+# base web socket url
+WS_URL = "wss://api.huobi.pro/ws"
 
 
+# function to get current time in unix format
 def get_unix_time():
     return round(time.time() * 1000)
 
 
-def get_trades(ws, message):
-    trade_data = json.loads(gzip.decompress(message))
+# function to format the trades output
+def get_trades(message):
+    trade_data = message
+    coin_name = trade_data['ch'].replace('market.', '').replace('.trade.detail', '')
+    for elem in trade_data['tick']['data']:
+        print('!', get_unix_time(),
+              size_symbols[coin_name],
+              str(elem['direction'])[:1].upper(), str('{0:.9f}'.format(elem['price'])),
+              str('{0:.4f}'.format(elem['amount'])), flush=True)
 
-    if 'ping' in trade_data:
-        ws.send(json.dumps({
-            "pong": trade_data['ping']
+
+# function to format order books and deltas(order book updates) format
+def get_order_books_and_deltas(message, update):
+    order_data = message
+    # check if bids array is not Null
+    coin_name = order_data['ch'].replace('market.', '').replace('.depth.step0', '')
+    answer = ''
+    if len(order_data['tick']['bids']) != 0:
+        answer += '$ ' + str(get_unix_time()) + ' '  + size_symbols[coin_name] + ' B '
+        pq = '|'.join(f"{str('{0:.10f}'.format(elem[1]))}@{str('{0:.8f}'.format(elem[0]))}"
+                      for elem in order_data['tick']['bids'])
+        if update:
+            print(answer + pq, flush=True)
+        else:
+            print(answer + pq + ' R', flush=True)
+    if len(order_data['tick']['asks']) != 0:
+        answer = ''
+        answer += '$ ' + str(get_unix_time()) + ' ' + size_symbols[coin_name] + ' S '
+        pq = '|'.join(f"{str('{0:.10f}'.format(elem[1]))}@{str('{0:.8f}'.format(elem[0]))}"
+                      for elem in order_data['tick']['asks'])
+        if update:
+            print(answer + pq, flush=True)
+        else:
+            print(answer + pq + ' R', flush=True)
+
+
+async def heartbeat(ws):
+    while True:
+        await ws.send(json.dumps({
+            "pong": get_unix_time()
         }))
-    elif 'ch' in trade_data:
-        coin_name = trade_data['ch'].replace('market.', '').replace('.trade.detail', '')
-        for elem in trade_data['tick']['data']:
-            lock.acquire()
-            print('!', get_unix_time(),
-                      'huobi', size_symbols[coin_name],
-                      str(elem['direction'])[:1].upper(), str('{0:.9f}'.format(elem['price'])),
-                      str('{0:.4f}'.format(elem['amount'])), flush=True)
-            lock.release()
+        await asyncio.sleep(5)
 
 
-def trade(ws):
+async def subscribe(ws):
     # create trades for each cryptocoin symbol
     for key, value in symbol_dict.items():
-        ws.send(json.dumps({
+        await ws.send(json.dumps({
             "sub": f"market.{key + value}.trade.detail",
             'id': '428550639'
         }))
-
-
-def order(ws):
-    for key, value in symbol_dict.items():
-        ws.send(json.dumps({
-            "sub": f"market.{key+value}.depth.step0",
+        await ws.send(json.dumps({
+            "sub": f"market.{key + value}.depth.step0",
+            'id': '428550639'
+        }))
+        await ws.send(json.dumps({
+            "sub": f"market.{key + value}.mbp.150",
             'id': '428550639'
         }))
 
 
-def get_orders(ws, message):
-    order_data = json.loads(gzip.decompress(message))
-
-    if 'ping' in order_data:
-        ws.send(json.dumps({
-            "pong": order_data['ping']
-        }))
-    elif 'ch' in order_data:
-        coin_name = order_data['ch'].replace('market.', '').replace('.depth.step0', '')
-        answer = ''
-        if len(order_data['tick']['bids']) != 0:
-            answer += '$ ' + str(get_unix_time()) + ' huobi ' + size_symbols[coin_name] + ' B '
-            pq = '|'.join(f"{str('{0:.10f}'.format(elem[1]))}@{str('{0:.8f}'.format(elem[0]))}"
-                               for elem in order_data['tick']['bids']) + ' R'
-            lock.acquire()
-            print(answer + pq, flush=True)
-            lock.release()
-        if len(order_data['tick']['asks']) != 0:
-            answer = ''
-            answer += '$ ' + str(get_unix_time()) + ' huobi ' + size_symbols[coin_name] + ' S '
-            pq = '|'.join(f"{str('{0:.10f}'.format(elem[1]))}@{str('{0:.8f}'.format(elem[0]))}"
-                               for elem in order_data['tick']['asks']) + ' R'
-            lock.acquire()
-            print(answer + pq, flush=True)
-            lock.release()
-
-
-def delta(ws):
-    for key, value in symbol_dict.items():
-        ws.send(json.dumps({
-            "sub": f"market.{key+value}.mbp.150",
-            'id': '428550639'
-        }))
-
-
-def get_deltas(ws, message):
-    delta_data = json.loads(gzip.decompress(message))
-
-    if 'ping' in delta_data:
-        ws.send(json.dumps({
-            "pong": delta_data['ping']
-        }))
-    elif 'ch' in delta_data:
-        coin_name = delta_data['ch'].replace('market.', '').replace('.mbp.150', '')
-        answer = ''
-        if delta_data['tick']['bids']:
-            answer += '$ ' + str(get_unix_time()) + ' huobi ' + size_symbols[coin_name] + ' B '
-            pq = '|'.join(f"{str('{0:.10f}'.format(elem[1]))}@{str('{0:.8f}'.format(elem[0]))}"
-                          for elem in delta_data['tick']['bids'])
-            lock.acquire()
-            print(answer + pq, flush=True)
-            lock.release()
-
-        if delta_data['tick']['asks']:
-            answer = ''
-            answer += '$ ' + str(get_unix_time()) + ' huobi ' + size_symbols[coin_name] + ' S '
-            pq = '|'.join(f"{str('{0:.10f}'.format(elem[1]))}@{str('{0:.8f}'.format(elem[0]))}"
-                          for elem in delta_data['tick']['asks'])
-            lock.acquire()
-            print(answer + pq, flush=True)
-            lock.release()
+async def main():
+    try:
+        # create connection with server via base ws url
+        async with websockets.connect(WS_URL, ping_interval=None) as ws:
+            sub_task = asyncio.create_task(subscribe(ws))
+            await sub_task
+            # create task to keep connection alive
+            pong = asyncio.create_task(heartbeat(ws))
+            while True:
+                # receiving data from server
+                data = await ws.recv()
+                # change format of received data to json format
+                dataJSON = json.loads(gzip.decompress(data))
+                try:
+                    # check if received data is about trades
+                    if 'trades' in dataJSON['ch']:
+                        get_trades(dataJSON)
+                    # check if received data is about updates on order book
+                    elif 'mbp' in dataJSON['ch']:
+                        get_order_books_and_deltas(dataJSON, update=True)
+                    # check if received data is about order books
+                    elif 'depth' in dataJSON['ch']:
+                        get_order_books_and_deltas(dataJSON, update=False)
+                    elif 'ping' in dataJSON:
+                        await ws.send(json.dumps({
+                            "pong": dataJSON['ping']
+                        }))
+                    else:
+                        print(dataJSON)
+                except Exception as e:
+                    print(f"Exception {e} occurred")
+                    # ws.close()
+    except Exception as conn_e:
+        print(f"WARNING: connection exception {conn_e} occurred")
 
 
-def main1(ws):
-    trade(ws)
-    while True:
-        message_trade = ws.recv()
-        try:
-            get_trades(ws, message_trade)
-        except:
-            pass
-
-
-def main2(ws):
-    order(ws)
-    while True:
-        message_order = ws.recv()
-        try:
-            get_orders(ws, message_order)
-        except:
-            pass
-
-
-def main3(ws):
-    delta(ws)
-    while True:
-        message_delta = ws.recv()
-        try:
-            get_deltas(ws, message_delta)
-        except:
-            pass
-
-
-def main():
-    ws = websocket.create_connection("wss://api.huobi.pro/ws")
-
-    t1 = threading.Thread(target=main1, args=(ws,))
-    t2 = threading.Thread(target=main2, args=(ws,))
-    t3 = threading.Thread(target=main3, args=(ws,))
-
-    t1.start()
-    t2.start()
-    t3.start()
-
-    t1.join()
-    t2.join()
-    t3.join()
-
-
-if __name__ == '__main__':
-    main()
+# run main function
+asyncio.run(main())
