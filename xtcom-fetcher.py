0b621fd59c83e96425ffd62d93235653b9fe7de2
bohdandr <bohdandr12@gmail.com>
2023-02-19 20:10:38 +0200

    xtcom-fetcher-comments

--- xtcom-fetcher.py
+++ xtcom-fetcher.py
@@ -4,21 +4,23 @@ import websockets
 import time
 import asyncio
 
+# get all available symbol pairs
 currency_url = 'https://sapi.xt.com/v4/public/symbol'
 answer = requests.get(currency_url)
 currencies = answer.json()
 list_currencies = list()
 WS_URL = 'wss://stream.xt.com/public'
 
+#check if the certain symbol pair is available
 for element in currencies["result"]["symbols"]:
 	if element["state"] == "ONLINE":
 		list_currencies.append(element["symbol"])
 
-
+#get time in unix format
 def get_unix_time():
 	return round(time.time() * 1000)
 
-
+#put the trade information in output format
 def get_trades(var):
 	trade_data = var
 	print('!', get_unix_time(), trade_data['data']['s'],
@@ -26,12 +28,14 @@ def get_trades(var):
 		  trade_data['data']["q"], flush=True)
 
 
+#put the orderbook and deltas information in output format
 def get_order_books(var, depth_update):
 	order_data = var
 	if 'a' in order_data['data'] and len(order_data["data"]["a"]) != 0:
 		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['data']['s'] + ' S '
 		pq = "|".join(el[0] + "@" + el[1] for el in order_data["data"]["a"])
 		answer = order_answer + pq
+		# checking if the input data is full orderbook or just update
 		if (depth_update == True):
 			print(answer)
 		else:
@@ -41,12 +45,14 @@ def get_order_books(var, depth_update):
 		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['data']['s'] + ' B '
 		pq = "|".join(el[0] + "@" + el[1] for el in order_data["data"]["b"])
 		answer = order_answer + pq
+		# checking if the input data is full orderbook or just update
 		if (depth_update == True):
 			print(answer)
 		else:
 			print(answer + " R")
 
 
+#process the situations when the server awaits "ping" request
 async def heartbeat(ws):
 	while True:
 		await ws.send(json.dumps({
@@ -56,10 +62,13 @@ async def heartbeat(ws):
 
 
 async def main():
+	# create connection with server via base ws url
 	async for ws in websockets.connect(WS_URL, ping_interval=None):
 		try:
+			# create task to keep connection alive
 			pong = asyncio.create_task(heartbeat(ws))
 			for i in range(len(list_currencies)):
+				#create the subscription for trades
 				await ws.send(json.dumps({
 					"method": "subscribe",
 					"params": [
@@ -68,6 +77,7 @@ async def main():
 					"id": "123"
 				}))
 
+				# create the subscription for full orderbooks
 				await ws.send(json.dumps({
 					"method": "subscribe",
 					"params": [
@@ -76,6 +86,7 @@ async def main():
 					"id": "123"
 				}))
 
+				# create the subscription for updates
 				await ws.send(json.dumps({
 					"method": "subscribe",
 					"params": [
@@ -90,12 +101,15 @@ async def main():
 				try:
 					dataJSON = json.loads(data)
 
+					#if received data is about trades
 					if dataJSON['topic'] == 'trade':
 						get_trades(dataJSON)
 
+					# if received data is about updates
 					elif dataJSON['topic'] == 'depth_update':
 						get_order_books(dataJSON, depth_update=True)
 
+					# if received data is about orderbooks
 					elif dataJSON['topic'] == 'depth':
 						get_order_books(dataJSON, depth_update=False)
 
