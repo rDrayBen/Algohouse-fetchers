d7d63a278901a1718e81054c3a521b37be34c763
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-04-09 22:20:56 +0300

    Add files via upload

---
+++ bkex-fetcher.js
@@ -0,0 +1,152 @@
+import WebSocket from 'ws';
+import fetch from 'node-fetch';
+
+
+// define the websocket and REST URLs
+const wsUrl = 'wss://fapi.bkex.com/fapi/v2/ws';
+const restUrl = "https://api.bkex.com/v2/common/symbols";
+
+
+// create a new websocket instance
+var ws = new WebSocket(wsUrl);
+
+
+const response = await fetch(restUrl);
+//extract JSON from the http response
+const myJson = await response.json(); 
+var currencies = [];
+
+
+// extract symbols from JSON returned information
+for(let i = 0; i < myJson['data'].length; ++i){
+    currencies.push(myJson['data'][i]['symbol']);
+}
+
+
+// print metadata about pairs
+async function Metadata(){
+    myJson['data'].forEach((item, index)=>{
+        let pair_data = '@MD ' + item['symbol'] + ' spot ' + item['symbol'].split('_')[0] + ' ' + item['symbol'].split('_')[1] + ' ' 
+        + item['pricePrecision'] + ' 1 1 0 0';
+        console.log(pair_data);
+    })
+    console.log('@MDEND')
+}
+
+
+//function to get current time in unix format
+function getUnixTime(){
+    return Math.floor(Date.now() / 1000);
+}
+
+
+// func to print trades
+async function getTrades(message){
+    var trade_output = '! ' + getUnixTime() + ' ' + 
+    message['type'].split('.')[0].toUpperCase() + ' ' + 
+    (message['data'][1] === '1' ? 'B ' : 'S ') + message['data'][0] + ' ' + message['data'][2];
+    console.log(trade_output);
+}
+
+
+// func to print orderbooks and deltas
+async function getOrders(message, update){
+    // check if bids array is not Null
+    if(message['data']['bids']){
+        var order_answer = '$ ' + getUnixTime() + ' ' + message['data']['symbol'].toUpperCase() + ' B '
+        var pq = '';
+        for(let i = 0; i < message['data']['bids'].length; i+=2){
+            pq += message['data']['bids'][i+1] + '@' + message['data']['bids'][i] + '|';
+        }
+        pq = pq.slice(0, -1);
+        // check if the input data is full order book or just update
+        if (update){
+            console.log(order_answer + pq)
+        }
+        else{
+            console.log(order_answer + pq + ' R')
+        }
+    }
+
+    // check if asks array is not Null
+    if(message['data']['asks']){
+        var order_answer = '$ ' + getUnixTime() + ' ' + message['data']['symbol'].toUpperCase() + ' S '
+        var pq = '';
+        for(let i = 0; i < message['data']['asks'].length; i+=2){
+            pq += message['data']['asks'][i+1] + '@' + message['data']['asks'][i] + '|';
+        }
+        pq = pq.slice(0, -1);
+        // check if the input data is full order book or just update
+        if (update){
+            console.log(order_answer + pq)
+        }
+        else{
+            console.log(order_answer + pq + ' R')
+        }
+    }
+}
+
+
+// call this func when first opening connection
+ws.onopen = function(e) {
+    // call metadata to execute
+    Metadata();
+    // create ping function to keep connection alive
+    ws.ping();
+    // subscribe to trades and orders for all instruments
+    currencies.forEach((item) =>{
+        // sub for trades
+        ws.send(JSON.stringify(
+            {
+                "event":"sub",
+                "topic":`${item.toLowerCase()}.trade`
+            }
+        ))
+        //sub for orderbooks with depth 20
+        ws.send(JSON.stringify(
+            {
+                "event":"sub",
+                "topic":`${item.toLowerCase()}.20deep`
+            }
+        ))
+        // sub for deltas in 5 top positions of orderbook
+        ws.send(JSON.stringify(
+            {
+                "event":"sub",
+                "topic":`${item.toLowerCase()}.updateDepth`
+            }
+        ))
+    })
+};
+
+
+// func to handle input messages
+ws.onmessage = function(event) {
+    // parse input data to JSON format
+    var dataJSON = JSON.parse(event.data);
+    if (dataJSON['type'].split('.')[1] === 'trade'){
+        getTrades(dataJSON);
+    }else if(dataJSON['type'].split('.')[1] === '20deep'){
+        getOrders(dataJSON, false);
+    }else if(dataJSON['type'].split('.')[1] === 'updateDepth'){
+        getOrders(dataJSON, true);
+    }else{
+        console.log(dataJSON);
+    }
+};
+
+
+// func to handle closing connection
+ws.onclose = function(event) {
+    if (event.wasClean) {
+        console.log(`Connection closed with code ${event.code} and reason ${event.reason}`);
+    } else {
+        console.log('Connection lost');
+    }
+};
+
+// func to handle errors
+ws.onerror = function(error) {
+    console.log(`Error ${error} occurred`);
+};
+
---
+++ trubit-pro-fetcher.py
@@ -0,0 +1,147 @@
+import json
+import requests
+import websockets
+import time
+import asyncio
+
+# get all available symbol pairs from exchange
+currency_url = 'https://api.mexo.io/openapi/v1/exchange'
+answer = requests.get(currency_url)
+currencies = answer.json()
+list_currencies = list()
+# base web socket url
+WS_URL = 'wss://wsapi.mexo.io/openapi/quote/ws/v1'
+precision = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000, 10000000000, 100000000000,
+             1000000000000, 10000000000000, 100000000000000, 1000000000000000, 10000000000000000]
+
+# fill the list with all available symbol pairs on exchange
+for pair_s in currencies['symbols']:
+    if pair_s['status'] == 'TRADING':
+        list_currencies.append(pair_s['symbol'])
+
+
+async def metadata():
+    for pair in currencies['symbols']:
+        if pair['status'] == 'TRADING':
+            prec = 11
+            for i in range(len(precision)):
+                if float(pair['quotePrecision']) * precision[i] == 1:
+                    prec = i
+            pair_data = '@MD ' + pair['symbol'] + ' spot ' + pair['baseAsset'] + ' ' + pair['quoteAsset'] + ' ' \
+                        + str(prec) + ' 1 1 0 0'
+            print(pair_data, flush=True)
+    print('@MDEND')
+
+
+# function to get current time in unix format
+def get_unix_time():
+    return round(time.time() * 1000)
+
+
+# function to format the trades output
+def get_trades(message):
+    for elem in message['data']:
+        print('!', get_unix_time(), message['symbol'],'B' if elem['m'] else 'S', elem['p'], elem['q'], flush=True)
+
+
+# function to format order books and deltas(order book updates) format
+def get_order_books_and_deltas(message, update):
+    # check if bids array is not Null
+    if 'b' in message['data'][0] and message['data'][0]['b']:
+        order_answer = '$ ' + str(get_unix_time()) + ' ' + message['symbol'] + ' B '
+        pq = '|'.join(f"{elem[1]}@{elem[0]}"
+                      for elem in message['data'][0]['b'])
+        # check if the input data is full order book or just update
+        if update:
+            print(order_answer + pq, flush=True)
+        elif not update:
+            print(order_answer + pq + ' R', flush=True)
+
+    # check if asks array is not Null
+    if 'a' in message['data'][0] and message['data'][0]['a']:
+        order_answer = '$ ' + str(get_unix_time()) + ' ' + message['symbol'] + ' S '
+        pq = '|'.join(f"{elem[1]}@{elem[0]}"
+                      for elem in message['data'][0]['a'])
+        # check if the input data is full order book or just update
+        if update:
+            print(order_answer + pq, flush=True)
+        elif not update:
+            print(order_answer + pq + ' R', flush=True)
+
+
+async def heartbeat(ws):
+    while True:
+        await ws.send(json.dumps({
+            "ping": get_unix_time()
+        }))
+        print(json.dumps({
+            "topic": "ping"
+        }))
+        await asyncio.sleep(10)
+
+
+async def subscribe(ws):
+    # subscribe for listed topics
+    for symbol in list_currencies:
+        await ws.send(json.dumps({
+          "symbol": f"{symbol}",
+          "topic": "trade",
+          "event": "sub",
+          "params": {
+            "binary": False
+          }
+        }))
+        await ws.send(json.dumps({
+          "symbol": f"{symbol}",
+          "topic": "diffDepth",
+          "event": "sub",
+          "params": {
+            "binary": False
+          }
+        }))
+
+
+async def main():
+    # create connection with server via base ws url
+    async for ws in websockets.connect(WS_URL):
+        try:
+            sub_task = asyncio.create_task(subscribe(ws))
+            # create task to keep connection alive
+            pong = asyncio.create_task(heartbeat(ws))
+            # print metadata about each pair symbols
+            meta_data = asyncio.create_task(metadata())
+            while True:
+                # receiving data from server
+                data = await ws.recv()
+                try:
+                    # change format of received data to json format
+                    dataJSON = json.loads(data)
+                    # check if received data is about trades
+                    if 'topic' in dataJSON and 'data' in dataJSON:
+                        if dataJSON['topic'] == 'trade' and not dataJSON['f']:
+                            get_trades(dataJSON)
+                        # skip history trade data
+                        elif dataJSON['topic'] == 'trade' and dataJSON['f']:
+                            pass
+                        # check if received data is about updates on order book
+                        elif dataJSON['topic'] == 'diffDepth' and not dataJSON['f']:
+                            get_order_books_and_deltas(dataJSON,
+                                                       update=True)
+                        # check if received data is about order books
+                        elif dataJSON['topic'] == 'diffDepth' and dataJSON['f']:
+                            get_order_books_and_deltas(dataJSON,
+                                                       update=False)
+                        else:
+                            print(dataJSON)
+                    elif 'error' in dataJSON:
+                        pass
+                    else:
+                        print(dataJSON)
+                except Exception as e:
+                    print(f"Exception {e} occurred")
+        except Exception as conn_e:
+            print(f"WARNING: connection exception {conn_e} occurred")
+
+
+# run main function
+asyncio.run(main())
