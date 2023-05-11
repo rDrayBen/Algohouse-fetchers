7e5c3ce45e2f11926bcc4d4af40c5e7d807acb14
Maksym Orlianskyi <87619450+OrlykM@users.noreply.github.com>
2023-05-11 23:09:33 +0300

    Tidex fetcher 1.1.0

--- tidex-fetcher.py
+++ tidex-fetcher.py
@@ -3,77 +3,97 @@ import websockets
 import time
 import asyncio
 import requests
-
 API_URL = "https://api.tidex.com"
 API_SYMBOLS = "/api/v1/public/markets"
-WS_URL = "wss://ws.tidex.com/"
+WSS_URL = "wss://ws.tidex.com/"
+TIMEOUT = 0.001
+PING_TIMEOUT = 5
+response = requests.get(API_URL + API_SYMBOLS)
+symbols = [i['name'] for i in response.json()['result']]
 
-def create_trades_message(symbols, id):
-    return json.dumps({
-      "method":"deals.subscribe",
-      "params":
-        symbols,
-      "id":id
-    })
-
-def create_orderbooks_message(symbol, limit, id):
-    return json.dumps({
-      "method":"depth.subscribe",
-      "params":
-        [
-          symbol,
-          limit,
-          "0"
-        ],
-      "id":id
-    })
-
-def print_trade(data):
+async def trades_subscribe(ws, data):
+    k = 1
+    await ws.send(json.dumps(
+                {
+                        "method": "deals.subscribe",
+                        "params": [data],
+                        "id": k
+                    }
+            ))
+    k+=1
+    await asyncio.sleep(TIMEOUT)
+async def orderbook_subscribe(ws, data):
+    k = 1000
+    await ws.send(json.dumps(
+                {
+                    "method": "depth.subscribe",
+                    "params": [data, 100, "0"],
+                    "id": k
+                }
+            ))
+    await asyncio.sleep(TIMEOUT)
+async def heartbeat(ws):
+    while True:
+        await ws.send(json.dumps({
+                "method": "server.ping",
+                "params": [],
+                "id": 0
+        }))
+        await asyncio.sleep(PING_TIMEOUT)
+async def meta(data):
+    for i in data['result']:
+        print("@MD", i['name'], "spot", i['stock'], i['money'], i['feePrec'], 1, 1, 0, 0, end='\n')
+    print("@MDEND")
+def print_trades(data):
     for i in data[1]:
-        if i['type'] == "sell":
-            print("!", round(time.time() *1000), data[0], "S", i['price'], i['amount'], end="\n")
-        else:
-            print("!", round(time.time() * 1000), data[0], "B", i['price'], i['amount'], end="\n")
+        print("!", round(time.time() * 1000), data[0], i['type'][0].upper(), i["price"], i['amount'], end="\n")
 
-def print_orderbook(data):
-    asks = data[1]['asks']
-    bids = data[1]['bids']
-    if data[0] == False:
-        # delta
-        if asks != []:
-            print("$", round(time.time() * 1000), data[2], "S", "|".join(i[1] + "@" + i[0] for i in asks), end="\n")
-        if bids != []:
-            print("$", round(time.time() * 1000), data[2], "B", "|".join(i[1] + "@" + i[0] for i in bids), end="\n")
-    elif data[0] == True:
-        #snapshot
-        if asks != []:
-            print("$", round(time.time() * 1000), data[2], "S", "|".join(i[1] + "@" + i[0] for i in asks),"R", end="\n")
-        if bids != []:
-            print("$", round(time.time() * 1000), data[2], "B", "|".join(i[1] + "@" + i[0] for i in bids),"R", end="\n")
+def print_orderbooks(data, isSnapshot):
+    if isSnapshot:
+        if "asks" in data['params'][1]:
+            print("$", round(time.time() * 1000), data['params'][2], "S",
+                  "|".join(str(i[1]) + "@" + str(i[0]) for i in data['params'][1]['asks']), "R", end="\n")
+        if "bids" in data['params'][1]:
+            print("$", round(time.time() * 1000), data['params'][2], "S",
+                  "|".join(str(i[1]) + "@" + str(i[0]) for i in data['params'][1]['asks']), "R", end="\n")
+    else:
+        if "asks" in data['params'][1]:
+            print("$", round(time.time() * 1000), data['params'][2], "S",
+                  "|".join(str(i[1]) + "@" + str(i[0]) for i in data['params'][1]['asks']), end="\n")
+        if "bids" in data['params'][1]:
+            print("$", round(time.time() * 1000), data['params'][2], "S",
+                  "|".join(str(i[1]) + "@" + str(i[0]) for i in data['params'][1]['asks']), end="\n")
 
-async def main():
-    response = requests.get(API_URL+ API_SYMBOLS)
-    symbols = [i['name'] for i in response.json()['result']]
-    trade_message = create_trades_message(symbols, 0)
-    orderbook_messages = [create_orderbooks_message(symbols[i], 30, i+1) for i in range(len(symbols))]
-    async with websockets.connect(WS_URL) as ws:
-        await ws.send(trade_message)
-        for i in orderbook_messages:
-            await ws.send(i)
-            time.sleep(0.047)
-        amount = 0
-        while True:
-            data = await ws.recv()
-            amount += 1
-            if amount >= 950:
-                await ws.send(json.dumps({"method":"server.ping","params":[], "id":1000}))
-                amount = 0
-            try:
-                dicted_data = json.loads(data)
-                if dicted_data["method"] == "deals.update":
-                    print_trade(dicted_data['params'])
-                if dicted_data["method"] == "depth.update":
-                    print_orderbook(dicted_data['params'])
-            except:
-                pass
-asyncio.run(main())
+async def handle_socket(symbol, ):
+    async for ws in websockets.connect(WSS_URL):
+        try:
+            orderbooks_sub_task = asyncio.create_task(orderbook_subscribe(ws, symbol))
+            trade_sub_task = asyncio.create_task(trades_subscribe(ws, symbol))
+            ping_task = asyncio.create_task(heartbeat(ws))
+
+            async for message in ws:
+                try:
+                    dataJSON = json.loads(message)
+                    if dataJSON['method'] == "depth.update" and dataJSON['params'][0] == False:
+                        if len(dataJSON['params'][1]['asks']) > 20:
+                            print_orderbooks(dataJSON, 1)
+                        else:
+                            print_orderbooks(dataJSON, 0)
+                    if dataJSON['method'] == "deals.update" and len(dataJSON['params'][1]) < 4:
+                        print_trades(dataJSON['params'])
+                except KeyboardInterrupt:
+                    exit(0)
+                except:
+                    pass
+        except KeyboardInterrupt:
+            exit(0)
+        except:
+            continue
+
+async def handler():
+    meta_task = asyncio.create_task(meta(response.json()))
+    await asyncio.wait([asyncio.create_task(handle_socket(symbol)) for symbol in symbols])
+
+def main():
+    asyncio.get_event_loop().run_until_complete(handler())
+main()
