35c459f7401b4b8ffb7945666feedf0cea033eeb
bohdandr <bohdandr12@gmail.com>
2023-10-31 17:16:39 +0200

    AD-131 solution

--- zondacrypto-fetcher.py
+++ zondacrypto-fetcher.py
@@ -4,6 +4,7 @@ import websockets
 import time
 import asyncio
 import os
+import sys
 
 currency_url = 'https://api.zondacrypto.exchange/rest/trading/ticker'
 answer = requests.get(currency_url)
@@ -13,6 +14,11 @@ WS_URL = 'wss://api.zondacrypto.exchange/websocket/'
 
 for key, value in currencies["items"].items():
 	list_currencies.append(key.lower())
+
+#for trades count stats
+symbol_count_for_5_minutes = {}
+for i in range(len(list_currencies)):
+	symbol_count_for_5_minutes[list_currencies[i]] = 0
 
 
 # get metadata about each pair of symbols
@@ -40,6 +46,7 @@ def get_trades(var):
 			print('!', get_unix_time(), symbol,
 				  "B" if elem["ty"] == "buy" else "S", elem['r'],
 				  elem["a"], flush=True)
+			symbol_count_for_5_minutes[symbol.lower()] += 1
 
 
 def get_order_books(var, update):
@@ -77,6 +84,10 @@ async def main():
 	# create connection with server via base ws url
 	async for ws in websockets.connect(WS_URL, ping_interval=None):
 		try:
+
+			start_time = time.time()
+			tradestats_time = start_time
+
 			# create task to keep connection alive
 			pong = asyncio.create_task(heartbeat(ws))
 
@@ -103,6 +114,15 @@ async def main():
 				data = await ws.recv()
 
 				dataJSON = json.loads(data)
+
+				if abs(time.time() - tradestats_time) >= 300:
+					data1 = "# LOG:CAT=trades_stats:MSG= "
+					data2 = " ".join(key.upper() + ":" + str(value) for key, value in symbol_count_for_5_minutes.items() if value != 0)
+					sys.stdout.write(data1 + data2)
+					sys.stdout.write("\n")
+					for key in symbol_count_for_5_minutes:
+						symbol_count_for_5_minutes[key] = 0
+					tradestats_time = time.time()
 
 				if dataJSON["action"] == 'push':
 
