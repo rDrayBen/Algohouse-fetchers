8dca24b33fae0019aaff7da90c9688f7ea5cbcfd
bohdandr <bohdandr12@gmail.com>
2023-10-31 15:41:36 +0200

    AD-131 solution

--- apexpro-fetcher.py
+++ apexpro-fetcher.py
@@ -4,15 +4,21 @@ import websockets
 import time
 import asyncio
 import os
+import sys
 
 currency_url = 'https://pro.apex.exchange/api/v1/symbols'
 answer = requests.get(currency_url)
 currencies = answer.json()
 list_currencies = list()
-
+WS_URL = f'wss://quote.pro.apex.exchange/realtime_public'
 
 for element in currencies["data"]["perpetualContract"]:
 	list_currencies.append(element["crossSymbolName"])
+
+#for trades count stats
+symbol_count_for_5_minutes = {}
+for i in range(len(list_currencies)):
+	symbol_count_for_5_minutes[list_currencies[i]] = 0
 
 
 # get metadata about each pair of symbols
@@ -30,14 +36,13 @@ async def metadata():
 def get_unix_time():
 	return round(time.time() * 1000)
 
-WS_URL = f'wss://quote.pro.apex.exchange/realtime_public'
-
 def get_trades(var):
 	trade_data = var
 	for elem in trade_data["data"]:
 		print('!', get_unix_time(), elem['s'],
 				"B" if elem["S"] == "Buy" else "S", elem['p'],
 				elem["v"], flush=True)
+		symbol_count_for_5_minutes[elem['s']] += 1
 
 
 def get_order_books(var, update):
@@ -77,6 +82,9 @@ async def main():
 	async for ws in websockets.connect(WS_URL, ping_interval=None):
 		try:
 
+			start_time = time.time()
+			tradestats_time = start_time
+
 			# create task to keep connection alive
 			pong = asyncio.create_task(heartbeat(ws))
 
@@ -103,6 +111,16 @@ async def main():
 				data = await ws.recv()
 
 				dataJSON = json.loads(data)
+
+				if abs(time.time() - tradestats_time) >= 15:
+					data1 = "# LOG:CAT=trades_stats:MSG= "
+					data2 = " ".join(key.upper() + ":" + str(value) for key, value in symbol_count_for_5_minutes.items() if value != 0)
+					sys.stdout.write(data1 + data2)
+					sys.stdout.write("\n")
+					for key in symbol_count_for_5_minutes:
+						symbol_count_for_5_minutes[key] = 0
+					tradestats_time = time.time()
+
 
 				if "topic" in dataJSON:
 
