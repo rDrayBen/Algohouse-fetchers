bc01f3c0afe50d1c9ae8a013b477dc8f0d5a10f9
bohdandr <bohdandr12@gmail.com>
2023-10-31 16:41:49 +0200

    AD-131 solution

--- hollaex-fetcher.py
+++ hollaex-fetcher.py
@@ -4,6 +4,7 @@ import websockets
 import time
 import asyncio
 import os
+import sys
 
 currency_url = 'https://api.hollaex.com/v2/constants'
 answer = requests.get(currency_url)
@@ -14,6 +15,11 @@ WS_URL = 'wss://api.hollaex.com/stream'
 for key, value in currencies["pairs"].items():
 	if value["is_public"]==True:
 		list_currencies.append(key)
+
+#for trades count stats
+symbol_count_for_5_minutes = {}
+for i in range(len(list_currencies)):
+	symbol_count_for_5_minutes[list_currencies[i]] = 0
 
 
 # get metadata about each pair of symbols
@@ -37,6 +43,7 @@ def get_trades(var):
 		print('!', get_unix_time(), trade_data["symbol"],
 			"B" if elem["side"] == "buy" else "S", str(elem["price"]),
 			 elem["size"], flush=True)
+		symbol_count_for_5_minutes[trade_data["symbol"]] += 1
 
 
 def get_order_books(var, update):
@@ -68,6 +75,10 @@ async def main():
 	# create connection with server via base ws url
 	async for ws in websockets.connect(WS_URL, ping_interval=None):
 		try:
+
+			start_time = time.time()
+			tradestats_time = start_time
+
 			# create task to keep connection alive
 			pong = asyncio.create_task(heartbeat(ws))
 
@@ -85,6 +96,15 @@ async def main():
 				data = await ws.recv()
 
 				dataJSON = json.loads(data)
+
+				if abs(time.time() - tradestats_time) >= 300:
+					data1 = "# LOG:CAT=trades_stats:MSG= "
+					data2 = " ".join(key.upper() + ":" + str(value) for key, value in symbol_count_for_5_minutes.items() if value != 0)
+					sys.stdout.write(data1 + data2)
+					sys.stdout.write("\n")
+					for key in symbol_count_for_5_minutes:
+						symbol_count_for_5_minutes[key] = 0
+					tradestats_time = time.time()
 
 				if "topic" in dataJSON:
 
