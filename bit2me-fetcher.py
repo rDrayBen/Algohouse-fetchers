8f38738fd21784ece34a5b13717543fb3531f782
bohdandr <bohdandr12@gmail.com>
2023-11-03 16:21:58 +0200

    finished

---
+++ bit2me-fetcher.py
@@ -0,0 +1,172 @@
+import json
+import requests
+import websockets
+import time
+import asyncio
+import sys
+
+# get all available symbol pairs
+currency_url = 'https://gateway.bit2me.com/v1/trading/market-config'
+answer = requests.get(currency_url)
+currencies = answer.json()
+list_currencies = list()
+WS_URL = 'wss://ws.bit2me.com/v1/trading'
+is_subscribed = {}
+# check if the certain symbol pair is available
+for element in currencies:
+	list_currencies.append(element["symbol"])
+	is_subscribed[element["symbol"]] = False
+
+#for trades count stats
+symbol_trade_count_for_5_minutes = {}
+for i in range(len(list_currencies)):
+	symbol_trade_count_for_5_minutes[list_currencies[i]] = 0
+
+#for orderbooks count stats
+symbol_orderbook_count_for_5_minutes = {}
+for i in range(len(list_currencies)):
+	symbol_orderbook_count_for_5_minutes[list_currencies[i]] = 0
+
+
+async def subscribe(ws):
+	while True:
+		for key, value in is_subscribed.items():
+
+			if value == False:
+
+				# create the subscription for trades
+				await ws.send(json.dumps({"event":"subscribe","subscription":{"name":f"{key}"}}))
+				await asyncio.sleep(0.1)
+
+
+		for el in list(is_subscribed):
+			is_subscribed[el] = False
+
+
+		await asyncio.sleep(2000)
+
+
+# get metadata about each pair of symbols
+async def metadata():
+	for pair in currencies:
+		pair_data = '@MD ' + pair["symbol"].split("/")[0] + '/' + pair["symbol"].split("/")[1] + ' spot ' + \
+					pair["symbol"].split("/")[0] + ' ' + pair["symbol"].split("/")[0] + \
+					' ' + str(pair['pricePrecision']) + ' 1 1 0 0'
+
+		print(pair_data, flush=True)
+
+	print('@MDEND')
+
+
+# get time in unix format
+def get_unix_time():
+	return round(time.time() * 1000)
+
+
+# put the trade information in output format
+def get_trades(var):
+	trade_data = var
+	print('!', get_unix_time(), trade_data["pair"],
+		  "B" if trade_data['data'][0] else "S", trade_data['data'][1],
+		  trade_data['data'][2], flush=True)
+	symbol_trade_count_for_5_minutes[trade_data["pair"]] += 1
+
+
+# put the orderbook and deltas information in output format
+def get_order_books(var, depth_update):
+	order_data = var
+	if 'asks' in order_data['data'] and len(order_data["data"]["asks"]) != 0:
+		symbol_orderbook_count_for_5_minutes[order_data['pair']] += len(order_data["data"]["asks"])
+		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['pair'] + ' S '
+		pq = "|".join(str(el[1]) + "@" + str(el[0]) for el in order_data["data"]["asks"])
+		answer = order_answer + pq
+		# checking if the input data is full orderbook or just update
+		if (depth_update == True):
+			print(answer)
+		else:
+			print(answer + " R")
+	if 'bids' in order_data['data'] and len(order_data["data"]["bids"]) != 0:
+		symbol_orderbook_count_for_5_minutes[order_data['pair']] += len(order_data["data"]["bids"])
+		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['pair'] + ' B '
+		pq = "|".join(str(el[1]) + "@" + str(el[0]) for el in order_data["data"]["bids"])
+		answer = order_answer + pq
+		# checking if the input data is full orderbook or just update
+		if (depth_update == True):
+			print(answer)
+		else:
+			print(answer + " R")
+
+
+# process the situations when the server awaits "ping" request
+async def heartbeat(ws):
+	while True:
+		await ws.send(json.dumps({"event":"ping"}))
+		await asyncio.sleep(5)
+
+
+async def main():
+	# create connection with server via base ws url
+	async for ws in websockets.connect(WS_URL, ping_interval=None):
+		try:
+
+			start_time = time.time()
+			tradestats_time = start_time
+
+			# create task to subscribe to symbols` pair
+			subscription = asyncio.create_task(subscribe(ws))
+
+			# create task to keep connection alive
+			pong = asyncio.create_task(heartbeat(ws))
+
+			# create task to get metadata about each pair of symbols
+			meta_data = asyncio.create_task(metadata())
+
+			print(meta_data)
+
+
+			while True:
+
+				data = await ws.recv()
+				dataJSON = json.loads(data)
+
+				# trade and orderbook stats output
+				if abs(time.time() - tradestats_time) >= 300:
+					data1 = "# LOG:CAT=trades_stats:MSG= "
+					data2 = " ".join(
+						key.upper() + ":" + str(value) for key, value in symbol_trade_count_for_5_minutes.items() if
+						value != 0)
+					sys.stdout.write(data1 + data2)
+					sys.stdout.write("\n")
+					for key in symbol_trade_count_for_5_minutes:
+						symbol_trade_count_for_5_minutes[key] = 0
+
+					data3 = "# LOG:CAT=orderbooks_stats:MSG= "
+					data4 = " ".join(
+						key.upper() + ":" + str(value) for key, value in symbol_orderbook_count_for_5_minutes.items() if
+						value != 0)
+					sys.stdout.write(data3 + data4)
+					sys.stdout.write("\n")
+					for key in symbol_orderbook_count_for_5_minutes:
+						symbol_orderbook_count_for_5_minutes[key] = 0
+
+					tradestats_time = time.time()
+
+				if "event" in dataJSON:
+					try:
+						# if received data is about trades
+						if dataJSON['event'] == 'last-trade':
+							is_subscribed[dataJSON['pair']] = True
+							get_trades(dataJSON)
+						# if received data is about updates
+						elif dataJSON['event'] == 'order-book':
+							is_subscribed[dataJSON['pair']] = True
+							get_order_books(dataJSON, depth_update=False)
+						else:
+							pass
+					except Exception as ex:
+						print(f"Exception {ex} occurred")
+		except Exception as conn_ex:
+			print(f"Connection exception {conn_ex} occurred")
+
+
+asyncio.run(main())
