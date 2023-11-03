0d74e40306be020f3d9b8198292dac668f779588
bohdandr <bohdandr12@gmail.com>
2023-11-03 13:43:54 +0200

    AD-132 solution

--- exmarkets-fetcher.py
+++ exmarkets-fetcher.py
@@ -13,17 +13,20 @@ WS_URL = 'wss://exmarkets.com/ws'
 list_currencies_id = list()
 list_currencies_name = list()
 
-print(currencies)
-
 
 for element in currencies["markets"]:
 	list_currencies_id.append(element["id"])
 	list_currencies_name.append(element["name"])
 
 #for trades count stats
-symbol_count_for_5_minutes = {}
+symbol_trade_count_for_5_minutes = {}
 for i in range(len(list_currencies_name)):
-	symbol_count_for_5_minutes[list_currencies_name[i].upper()] = 0
+	symbol_trade_count_for_5_minutes[list_currencies_name[i].upper()] = 0
+
+#for orderbooks count stats
+symbol_orderbook_count_for_5_minutes = {}
+for i in range(len(list_currencies_name)):
+	symbol_orderbook_count_for_5_minutes[list_currencies_name[i].upper()] = 0
 
 async def subscribe(ws, symbol):
 
@@ -63,12 +66,13 @@ def get_trades(var):
 		print('!', get_unix_time(), trade_data["data"]["market"].upper(),
 				"S" if trade_data["data"]["side"] == "SELL" else "B", str(format(trade_data["data"]['price'], '.5f')),
 				str(trade_data["data"]['amount']), flush=True)
-		symbol_count_for_5_minutes[trade_data["data"]["market"].upper()] += 1
+		symbol_trade_count_for_5_minutes[trade_data["data"]["market"].upper()] += 1
 
 
 def get_order_books(var):
 	order_data = var
 	if 'sell' in order_data['data'] and len(order_data["data"]["sell"]) != 0:
+		symbol_orderbook_count_for_5_minutes[order_data['data']['market'].upper()] += len(order_data["data"]["sell"])
 		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['data']['market'].upper() + ' S '
 		pq = "|".join(str(el['amount']) + "@" + str(format(el['price'], '.5f')) for el in order_data["data"]["sell"])
 		answer = order_answer + pq
@@ -76,6 +80,7 @@ def get_order_books(var):
 		print(answer + " R")
 
 	if 'buy' in order_data['data'] and len(order_data["data"]["buy"]) != 0:
+		symbol_orderbook_count_for_5_minutes[order_data['data']['market'].upper()] += len(order_data["data"]["buy"])
 		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['data']['market'].upper() + ' B '
 		pq = "|".join(str(el['amount']) + "@" + str(format(el['price'], '.5f')) for el in order_data["data"]["buy"])
 		answer = order_answer + pq
@@ -85,16 +90,27 @@ def get_order_books(var):
 
 async def stats():
 	while True:
-		data1 = "# LOG:CAT=trades_stats:MSG= "
-		data2 = " ".join(
-			key.upper() + ":" + str(value) for key, value in symbol_count_for_5_minutes.items() if
-			value != 0)
-		sys.stdout.write(data1 + data2)
-		sys.stdout.write("\n")
-		for key in symbol_count_for_5_minutes:
-			symbol_count_for_5_minutes[key] = 0
+		while True:
+			data1 = "# LOG:CAT=trades_stats:MSG= "
+			data2 = " ".join(
+				key.upper() + ":" + str(value) for key, value in symbol_trade_count_for_5_minutes.items() if
+				value != 0)
+			sys.stdout.write(data1 + data2)
+			sys.stdout.write("\n")
+			for key in symbol_trade_count_for_5_minutes:
+				symbol_trade_count_for_5_minutes[key] = 0
 
-		await asyncio.sleep(300)
+			data3 = "# LOG:CAT=orderbooks_stats:MSG= "
+			data4 = " ".join(
+				key.upper() + ":" + str(value) for key, value in
+				symbol_orderbook_count_for_5_minutes.items() if
+				value != 0)
+			sys.stdout.write(data3 + data4)
+			sys.stdout.write("\n")
+			for key in symbol_orderbook_count_for_5_minutes:
+				symbol_orderbook_count_for_5_minutes[key] = 0
+
+			await asyncio.sleep(300)
 
 async def socket(symbol):
 	# create connection with server via base ws url
