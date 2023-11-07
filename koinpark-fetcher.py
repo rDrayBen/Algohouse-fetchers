6dd131a5181aed14f3682d310fdeb4c94fc3024b
bohdandr <bohdandr12@gmail.com>
2023-11-07 12:51:17 +0200

    AD-153 solution

--- koinpark-fetcher.py
+++ koinpark-fetcher.py
@@ -3,15 +3,30 @@ import requests
 import asyncio
 import time
 import websockets
+import sys
+import os
 
-API_URL = 'https://sapi.xt.com/v4'
-API_SPOT_SYMBOLS_URL = '/public/symbol'
+API_URL = 'https://api.koinpark.com'
+API_SPOT_SYMBOLS_URL = '/public_api/markets'
 WS_URL = 'wss://knprklvtrdkand.koinpark.com/socket.io/?EIO=4&transport=websocket'
+is_subscribed_orderbooks = {}
+is_subscribed_trades = {}
 
 TIMEOUT = 0.1
 PING_TIMEOUT = 25
 response = requests.get(API_URL + API_SPOT_SYMBOLS_URL)
-symbols = [x['symbol'] for x in response.json()['result']['symbols']]
+symbols = [x['trading_pairs'] for x in response.json()['data']]
+
+#for trades count stats
+symbol_trade_count_for_5_minutes = {}
+for i in range(len(symbols)):
+	symbol_trade_count_for_5_minutes[symbols[i].upper()] = 0
+
+#for orderbooks count stats
+symbol_orderbook_count_for_5_minutes = {}
+for i in range(len(symbols)):
+	symbol_orderbook_count_for_5_minutes[symbols[i].upper()] = 0
+
 
 # get time in unix format
 def get_unix_time():
@@ -19,10 +34,9 @@ def get_unix_time():
 
 # get metadata about each pair of symbols
 async def metadata():
-	for pair in response.json()["result"]["symbols"]:
-		pair_data = '@MD ' + pair["baseCurrency"].upper() + '_' + pair["quoteCurrency"].upper() + ' spot ' + \
-					pair["baseCurrency"].upper() + ' ' + pair["quoteCurrency"].upper() + \
-					' ' + str(pair['pricePrecision']) + ' 1 1 0 0'
+	for pair in response.json()["data"]:
+		pair_data = '@MD ' + pair["trading_pairs"].split("_")[0] + '_' + pair["trading_pairs"].split("_")[1] + ' spot ' + \
+					pair["trading_pairs"] + '-1 1 1 0 0'
 
 		print(pair_data, flush=True)
 
@@ -41,6 +55,7 @@ def get_trades(var):
 	print('!', get_unix_time(), trade_data[0].split("_", 1)[1],
 		  "B" if trade_data[1]['data']["isBuyerMaker"] else "S", trade_data[1]['data']['price'],
 		  trade_data[1]['data']["amount"], flush=True)
+	symbol_trade_count_for_5_minutes[trade_data[0].split("_", 1)[1]] += 1
 
 
 # put the orderbook and deltas information in output format
@@ -51,11 +66,13 @@ def get_order_books(var, depth_update):
 	iteration = 0
 	for elem in order_data[1]['data']['changes']:
 		if 'sell' in elem and len(elem) != 0:
+			symbol_orderbook_count_for_5_minutes[order_data[1]['productId']] += 1
 			if iteration == 0:
 				pq_S += elem[2] + "@" + elem[1]
 			else:
 				pq_S += "|" + elem[2] + "@" + elem[1]
 		if 'buy' in elem and len(elem) != 0:
+			symbol_orderbook_count_for_5_minutes[order_data[1]['productId']] += 1
 			if iteration == 0:
 				pq_B += elem[2] + "@" + elem[1]
 			else:
@@ -83,6 +100,8 @@ async def main():
 	meta_task = asyncio.create_task(metadata())
 	async for ws in websockets.connect(WS_URL):
 		try:
+			start_time = time.time()
+			tradestats_time = start_time
 			sub_task = asyncio.create_task(subscribe(ws))
 			ping_task = asyncio.create_task(heartbeat(ws))
 			while True:
@@ -92,12 +111,38 @@ async def main():
 						data_sliced = data[2:]
 						dataJSON = json.loads(data_sliced)
 
+						# trade and orderbook stats output
+						if abs(time.time() - tradestats_time) >= 300:
+							data1 = "# LOG:CAT=trades_stats:MSG= "
+							data2 = " ".join(
+								key.upper() + ":" + str(value) for key, value in
+								symbol_trade_count_for_5_minutes.items() if
+								value != 0)
+							sys.stdout.write(data1 + data2)
+							sys.stdout.write("\n")
+							for key in symbol_trade_count_for_5_minutes:
+								symbol_trade_count_for_5_minutes[key] = 0
+
+							data3 = "# LOG:CAT=orderbooks_stats:MSG= "
+							data4 = " ".join(
+								key.upper() + ":" + str(value) for key, value in
+								symbol_orderbook_count_for_5_minutes.items() if
+								value != 0)
+							sys.stdout.write(data3 + data4)
+							sys.stdout.write("\n")
+							for key in symbol_orderbook_count_for_5_minutes:
+								symbol_orderbook_count_for_5_minutes[key] = 0
+
+							tradestats_time = time.time()
+
 						# if received data is about trades
 						if "tradehistory" in dataJSON[0]:
 							get_trades(dataJSON)
 						# if received data is about orderbooks
 						elif "orderBookMatch" in dataJSON[0]:
-							get_order_books(dataJSON, depth_update=True)
+							# possibility to not subscribe or report orderbook changes
+							if os.getenv("SKIP_ORDERBOOKS") == None:
+								get_order_books(dataJSON, depth_update=True)
 						else:
 							pass
 				except:
