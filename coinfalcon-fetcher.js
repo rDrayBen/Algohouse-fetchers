38d51fa7e610b81d52cffb0512230ec1328e3a1d
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-11-09 12:49:06 +0200

    Fixed AD160 ticket

--- coinfalcon-fetcher.js
+++ coinfalcon-fetcher.js
@@ -133,6 +133,7 @@ async function stats(){
     if (stat_line !== '# LOG:CAT=orderbook_stats:MSG= '){
         console.log(stat_line);
     }
+    setTimeout(stats, 300000);
 }
 
 
@@ -208,6 +209,5 @@ async function Connect(){
 }
 
 Metadata();
-stats();
-setInterval(stats, 300000);
+setTimeout(stats, parseFloat(5 - ((Date.now() / 60000) % 5)) * 60000);
 await Connect();
