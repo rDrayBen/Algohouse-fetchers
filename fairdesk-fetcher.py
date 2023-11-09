823feb232ecbeca561420b827276851dd15e3719
bohdandr <bohdandr12@gmail.com>
2023-11-09 18:07:47 +0200

    symbols output fixed + SKIP SNAPSHOT TRADES condition added

--- fairdesk-fetcher.py
+++ fairdesk-fetcher.py
@@ -25,18 +25,18 @@ tickSize = [0.1, 0.01, 0.01, 1.0E-5, 0.01, 1.0E-4, 1.0E-5, 0.001, 1.0E-4, 0.001,
 #for trades count stats
 symbol_trade_count_for_5_minutes = {}
 for i in range(len(list_currencies)):
-	symbol_trade_count_for_5_minutes[list_currencies[i].lower()] = 0
+	symbol_trade_count_for_5_minutes[list_currencies[i].upper()] = 0
 
 #for orderbooks count stats
 symbol_orderbook_count_for_5_minutes = {}
 for i in range(len(list_currencies)):
-	symbol_orderbook_count_for_5_minutes[list_currencies[i].lower()] = 0
+	symbol_orderbook_count_for_5_minutes[list_currencies[i]] = 0
 
 # get metadata about each pair of symbols
 async def metadata():
 	for i in range(len(list_currencies)):
-		pair_data = '@MD ' + list_currencies[i].lower() + ' spot ' + \
-					baseCcyName[i].lower() + ' ' + quoteCcyName.lower() + \
+		pair_data = '@MD ' + list_currencies[i] + ' spot ' + \
+					baseCcyName[i] + ' ' + quoteCcyName + \
 					' ' + str(str(tickSize[i])[::-1].find('.')) + ' 1 1 0 0'
 
 		print(pair_data, flush=True)
@@ -51,11 +51,11 @@ def get_unix_time():
 def get_trades(var, start_time):
 	trade_data = var
 	elapsed_time = time.time() - start_time
-	if elapsed_time > 3:
-		print('!', get_unix_time(), trade_data['s'],
+	if elapsed_time > 5:
+		print('!', get_unix_time(), trade_data['s'].upper(),
 			  'B' if trade_data['m'] else 'S', trade_data['p'],
 			  trade_data["q"], flush=True)
-		symbol_trade_count_for_5_minutes[trade_data['s']] += 1
+		symbol_trade_count_for_5_minutes[trade_data['s'].upper()] += 1
 		# print("symbol_count_for_5_minutes[trade_data['s']]:", trade_data['s'])
 		# print(f"symbol_count_for_5_minutes[trade_data['s']] for el {trade_data['s']}:",
 		# 	trade_data['s'], symbol_count_for_5_minutes[trade_data['s']])
@@ -65,16 +65,16 @@ def get_trades(var, start_time):
 def get_order_books(var, update):
 	order_data = var
 	if 'a' in order_data and len(order_data["a"]) != 0:
-		symbol_orderbook_count_for_5_minutes[order_data['s']] += len(order_data["a"])
-		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['s'] + ' S '
+		symbol_orderbook_count_for_5_minutes[order_data['s'].upper()] += len(order_data["a"])
+		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['s'].upper() + ' S '
 		pq = "|".join(el[1] + "@" + el[0] for el in order_data["a"])
 		answer = order_answer + pq
 
 		print(answer + " R")
 
 	if 'b' in order_data and len(order_data["b"]) != 0:
-		symbol_orderbook_count_for_5_minutes[order_data['s']] += len(order_data["b"])
-		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['s'] + ' B '
+		symbol_orderbook_count_for_5_minutes[order_data['s'].upper()] += len(order_data["b"])
+		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['s'].upper() + ' B '
 		pq = "|".join(el[1] + "@" + el[0] for el in order_data["b"])
 		answer = order_answer + pq
 
