5b5a9d74cad2f09ca01386102bcb36496dcbc976
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-11-09 12:31:25 +0200

    Fixed AD160 ticket

--- bitvenus-fetcher.js
+++ bitvenus-fetcher.js
@@ -133,6 +133,7 @@ async function stats(){
     if (stat_line !== '# LOG:CAT=orderbook_stats:MSG= '){
         console.log(stat_line);
     }
+    setTimeout(stats, 300000);
 }
 
 
@@ -281,8 +282,7 @@ async function ConnectOrders(){
 }
 
 Metadata();
-stats();
-setInterval(stats, 300000);
+setTimeout(stats, parseFloat(5 - ((Date.now() / 60000) % 5)) * 60000);
 await ConnectTrades();
 if(getenv.string("SKIP_ORDERBOOKS", '') === '' || getenv.string("SKIP_ORDERBOOKS") === null){
     await ConnectOrders();
