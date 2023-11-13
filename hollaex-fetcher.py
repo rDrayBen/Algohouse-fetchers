1bf369ee50104339ea4e5bd7f0f93b779f0ee190
bohdandr <bohdandr12@gmail.com>
2023-11-13 12:48:39 +0200

    symbol pairs output fixed

--- hollaex-fetcher.py
+++ hollaex-fetcher.py
@@ -14,24 +14,24 @@ WS_URL = 'wss://api.hollaex.com/stream'
 
 for key, value in currencies["pairs"].items():
 	if value["is_public"]==True:
-		list_currencies.append(key)
+		list_currencies.append(key.upper())
 
 #for trades count stats
 symbol_trade_count_for_5_minutes = {}
 for i in range(len(list_currencies)):
-	symbol_trade_count_for_5_minutes[list_currencies[i]] = 0
+	symbol_trade_count_for_5_minutes[list_currencies[i].upper()] = 0
 
 #for orderbooks count stats
 symbol_orderbook_count_for_5_minutes = {}
 for i in range(len(list_currencies)):
-	symbol_orderbook_count_for_5_minutes[list_currencies[i]] = 0
+	symbol_orderbook_count_for_5_minutes[list_currencies[i].upper()] = 0
 
 
 # get metadata about each pair of symbols
 async def metadata():
 	for key, value in currencies["pairs"].items():
-		pair_data = '@MD ' + value["pair_base"] + "-" + value["pair_2"] + ' spot ' + \
-					value["pair_base"] + ' ' + value["pair_2"] + ' -1 1 1 0 0'
+		pair_data = '@MD ' + value["pair_base"].upper() + "-" + value["pair_2"].upper() + ' spot ' + \
+					value["pair_base"].upper() + ' ' + value["pair_2"].upper() + ' -1 1 1 0 0'
 
 		print(pair_data, flush=True)
 
@@ -45,25 +45,25 @@ def get_unix_time():
 def get_trades(var):
 	trade_data = var
 	for elem in trade_data["data"]:
-		print('!', get_unix_time(), trade_data["symbol"],
+		print('!', get_unix_time(), trade_data["symbol"].upper(),
 			"B" if elem["side"] == "buy" else "S", str(elem["price"]),
 			 elem["size"], flush=True)
-		symbol_trade_count_for_5_minutes[trade_data["symbol"]] += 1
+		symbol_trade_count_for_5_minutes[trade_data["symbol"].upper()] += 1
 
 
 def get_order_books(var):
 	order_data = var
 	if 'asks' in order_data["data"] and len(order_data["data"]["asks"]) != 0:
-		symbol_orderbook_count_for_5_minutes[order_data["symbol"]] += len(order_data["data"]["asks"])
-		order_answer = '$ ' + str(get_unix_time()) + " " + order_data["symbol"] + ' S '
+		symbol_orderbook_count_for_5_minutes[order_data["symbol"].upper()] += len(order_data["data"]["asks"])
+		order_answer = '$ ' + str(get_unix_time()) + " " + order_data["symbol"].upper() + ' S '
 		pq = "|".join(str(el[1]) + "@" + str("{:.8f}".format(el[0])) for el in order_data["data"]["asks"])
 		answer = order_answer + pq
 
 		print(answer + " R")
 
 	if 'bids' in order_data["data"] and len(order_data["data"]["bids"]) != 0:
-		symbol_orderbook_count_for_5_minutes[order_data["symbol"]] += len(order_data["data"]["bids"])
-		order_answer = '$ ' + str(get_unix_time()) + " " + order_data["symbol"] + ' B '
+		symbol_orderbook_count_for_5_minutes[order_data["symbol"].upper()] += len(order_data["data"]["bids"])
+		order_answer = '$ ' + str(get_unix_time()) + " " + order_data["symbol"].upper() + ' B '
 		pq = "|".join(str(el[1]) + "@" + str("{:.8f}".format(el[0])) for el in order_data["data"]["bids"])
 		answer = order_answer + pq
 
