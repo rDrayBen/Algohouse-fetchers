6d10fd836a710e90c5c57f513e463da823bc1cd5
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-11-20 12:45:33 +0200

    Fixed AD211 ticket

--- woo-fetcher.py
+++ woo-fetcher.py
@@ -4,41 +4,65 @@ import websockets
 import time
 import asyncio
 import os
+import sys
+
+CURRENT_MODE = 'SPOT'
+
+# Extract command-line arguments starting from index 1
+args = sys.argv[1:]
+
+# Check if there are any arguments
+if len(args) > 0:
+    # Iterate through the arguments
+    for arg in args:
+        # Check if the argument starts with '-'
+        if arg.startswith('-') and arg[1:] == 'perpetual':
+            CURRENT_MODE = 'FUTURES'
+            break
+else:
+    CURRENT_MODE = "SPOT"
+
 
 # get all available symbol pairs from exchange
-currency_url = 'https://api.woo.org/v1/public/info'
+currency_url = 'https://api.woo.org/v4/public/info'
+# base web socket url
+WS_URL = 'wss://wss.woo.org/ws/stream/1fbac7b8-d849-4f95-b2ef-cf988a95f4d3'
+# user id
+UI = '91648'
+
 answer = requests.get(currency_url)
 currencies = answer.json()
 list_currencies = list()
 check_activity = {}
 trades_count_5min = {}
 orders_count_5min = {}
-# base web socket url
-WS_URL = 'wss://wss.woo.org/ws/stream/1fbac7b8-d849-4f95-b2ef-cf988a95f4d3'
-# user id
-UI = '91648'
-precision = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
+
 
 # fill the list with available pairs
-for currency in currencies['rows']:
-    list_currencies.append(currency['symbol'][5:])
+for currency in currencies['data']['rows']:
+    if CURRENT_MODE == 'SPOT' and "SPOT" in currency['symbol']:
+        list_currencies.append(currency['symbol'])
+    elif CURRENT_MODE == 'FUTURES' and "PERP" in currency['symbol']:
+        list_currencies.append(currency['symbol'])
 
 
 async def metadata():
-    for pair in currencies['rows']:
-        prec = 0
-        for i in range(len(precision)):
-            if pair['base_tick'] * precision[i] == 1:
-                prec = i
-        pair_data = '@MD ' + pair['symbol'].split('_')[1] + '-' + pair['symbol'].split('_')[2] + ' spot ' + \
-                    pair['symbol'].split('_')[1] + ' ' + pair['symbol'].split('_')[2] + ' ' + \
-                    str(prec) + ' 1 1 0 0'
-        check_activity[pair['symbol'][5:]] = False
-        trades_count_5min[pair['symbol'].split('_')[1] + '-' + pair['symbol'].split('_')[2]] = 0
-        orders_count_5min[pair['symbol'].split('_')[1] + '-' + pair['symbol'].split('_')[2]] = 0
-        print(pair_data, flush=True)
+    for pair in currencies['data']['rows']:
+        if CURRENT_MODE == 'SPOT' and "SPOT" in pair['symbol']:
+            pair_data = '@MD ' + pair['symbol'] + ' spot ' + pair['symbol'].split('_')[1] + \
+                        ' ' + pair['symbol'].split('_')[2] + ' ' + str(str(pair['precisions'][-1]).count('0')) + ' 1 1 0 0'
+            check_activity[pair['symbol']] = False
+            trades_count_5min[pair['symbol']] = 0
+            orders_count_5min[pair['symbol']] = 0
+            print(pair_data, flush=True)
+        elif CURRENT_MODE == 'FUTURES' and "PERP" in pair['symbol']:
+            pair_data = '@MD ' + pair['symbol'] + ' perpetual ' + pair['symbol'].split('_')[1] + \
+                        ' ' + pair['symbol'].split('_')[2] + ' ' + str(str(pair['precisions'][-1]).count('0')) + ' 1 1 0 0'
+            check_activity[pair['symbol']] = False
+            trades_count_5min[pair['symbol']] = 0
+            orders_count_5min[pair['symbol']] = 0
+            print(pair_data, flush=True)
     print('@MDEND')
-    # print(check_activity)
 
 
 # function to get current time in unix format
@@ -46,25 +70,30 @@ def get_unix_time():
     return round(time.time() * 1000)
 
 
+def chunk_array(array, chunk_size):
+    result = [array[i:i + chunk_size] for i in range(0, len(array), chunk_size)]
+    return result
+
+
 # function to format the trades output
 def get_trades(message):
-    check_activity[message['symbol'][5:]] = True
-    trades_count_5min[message['symbol'].split('_')[1] + '-' + message['symbol'].split('_')[2]] += 1
-    print('!', get_unix_time(), message['symbol'].split('_')[1] + '-' + message['symbol'].split('_')[2],
-          message['side'][0].upper(), str('{0:.9f}'.format(message['price'])),
-          str('{0:.9f}'.format(message['size'])), flush=True)
+    for trade in message['data']:
+        check_activity[trade['s']] = True
+        trades_count_5min[trade['s']] += 1
+        print('!', get_unix_time(), trade['s'],
+              trade['b'][0].upper(), str('{0:.9f}'.format(trade['p'])),
+              str('{0:.9f}'.format(trade['a'])), flush=True)
 
 
 # function to format order books and deltas(order book updates) format
 def get_order_books_and_deltas(message, update):
-    check_activity[message['symbol'][5:]] = True
-    orders_count_5min[message['symbol'].split('_')[1] + '-' + message['symbol'].split('_')[2]] += len(message['bids']) + len(message['asks'])
+    check_activity[message['data']['symbol']] = True
+    orders_count_5min[message['data']['symbol']] += len(message['data']['bids']) + len(message['data']['asks'])
     # check if bids array is not Null
-    if message['bids']:
-        order_answer = '$ ' + str(get_unix_time()) + ' ' + message['symbol'].split('_')[1] + '-' +\
-                       message['symbol'].split('_')[2] + ' B '
+    if message['data']['bids'] and len(message['data']['bids']):
+        order_answer = '$ ' + str(get_unix_time()) + ' ' + message['data']['symbol'] + ' B '
         pq = '|'.join(f"{str('{0:.9f}'.format(elem[1]))}@{str('{0:.9f}'.format(elem[0]))}"
-                      for elem in message['bids'])
+                      for elem in message['data']['bids'])
         # check if the input data is full order book or just update
         if update:
             print(order_answer + pq, flush=True)
@@ -74,11 +103,10 @@ def get_order_books_and_deltas(message, update):
     order_answer = ''
     pq = ''
     # check if asks array is not Null
-    if message['asks']:
-        order_answer = '$ ' + str(get_unix_time()) + ' ' + message['symbol'].split('_')[1] + '-' +\
-                       message['symbol'].split('_')[2] + ' S '
+    if message['data']['asks'] and len(message['data']['asks']):
+        order_answer = '$ ' + str(get_unix_time()) + ' ' + message['data']['symbol'] + ' S '
         pq = '|'.join(f"{str('{0:.9f}'.format(elem[1]))}@{str('{0:.9f}'.format(elem[0]))}"
-                      for elem in message['asks'])
+                      for elem in message['data']['asks'])
         # check if the input data is full order book or just update
         if update:
             print(order_answer + pq, flush=True)
@@ -94,27 +122,31 @@ async def heartbeat(ws):
         await asyncio.sleep(9)
 
 
-async def subscribe(ws, symb):
+async def subscribe(ws, symb_chunk):
     # subscribe for listed topics
-    # subscribe to all trades
-    await ws.send(json.dumps({
-        "id": f"{UI}",
-        "topic": f"SPOT_{symb}@trade",
-        "event": "subscribe"
-    }))
-    if os.getenv("SKIP_ORDERBOOKS") is None or os.getenv("SKIP_ORDERBOOKS") == '':
-        # subscribe to all order books
+    for symb in symb_chunk:
+        # subscribe to all trades
         await ws.send(json.dumps({
-            "id": f"{UI}",
-            "topic": f"SPOT_{symb}@orderbook",
+            "id": f"{symb}@trades",
+            "topic": f"{symb}@trades",
             "event": "subscribe"
         }))
-        # subscribe to all order book updates
-        await ws.send(json.dumps({
-            "id": f"{UI}",
-            "topic": f"SPOT_{symb}@orderbookupdate",
-            "event": "subscribe"
-        }))
+        if os.getenv("SKIP_ORDERBOOKS") is None or os.getenv("SKIP_ORDERBOOKS") == '':
+            # subscribe to all order books
+            await ws.send(json.dumps({
+                "id": "orderbook",
+                "event": "request",
+                "params": {
+                    "type": "orderbook",
+                    "symbol": symb
+                }
+            }))
+            # subscribe to all order book updates
+            await ws.send(json.dumps({
+                "id": f"{symb}@orderbookupdate",
+                "topic": f"{symb}@orderbookupdate",
+                "event": "subscribe"
+            }))
 
 
 async def stats():
@@ -139,11 +171,11 @@ async def stats():
         await asyncio.sleep(300)
 
 
-async def connect(symbol):
+async def connect(symbol_chunk):
     # create connection with server via base ws url
     async for ws in websockets.connect(WS_URL, ping_interval=9):
         try:
-            sub_task = asyncio.create_task(subscribe(ws, symbol))
+            sub_task = asyncio.create_task(subscribe(ws, symbol_chunk))
             # create task to keep connection alive
             pong = asyncio.create_task(heartbeat(ws))
 
@@ -155,14 +187,11 @@ async def connect(symbol):
                 try:
                     if 'topic' in dataJSON:
                         # check if received data is about trades
-                        if 'trade' in dataJSON['topic']:
-                            get_trades(dataJSON['data'])
-                        # check if received data is about order books
-                        elif 'orderbookupdate' in dataJSON['topic']:
-                            get_order_books_and_deltas(dataJSON['data'], update=True)
+                        if 'trades' in dataJSON['topic']:
+                            get_trades(dataJSON)
                         # check if received data is about updates on order book
-                        elif 'orderbook' in dataJSON['topic']:
-                            get_order_books_and_deltas(dataJSON['data'], update=False)
+                        elif 'orderbookupdate' in dataJSON['topic']:
+                            get_order_books_and_deltas(dataJSON, update=True)
                         else:
                             print(dataJSON)
                     # sending ping to keep connection alive
@@ -171,10 +200,13 @@ async def connect(symbol):
                             'event': "pong",
                             "ts": get_unix_time()
                         }))
+                    # check if received data is about order books
+                    elif dataJSON['id'] == 'orderbook' and dataJSON['event'] == 'request':
+                        get_order_books_and_deltas(dataJSON, update=False)
                     else:
                         print(dataJSON)
                 except Exception as e:
-                    print(f"Exception {e} occurred")
+                    print(f"Exception {e} occurred", data)
         except Exception as conn_e:
             print(f"WARNING: connection exception {conn_e} occurred")
 
@@ -184,13 +216,13 @@ async def connectionHandler():
     meta_data = asyncio.create_task(metadata())
     # print stats for trades and orders
     statistics = asyncio.create_task(stats())
+    chunked_array = chunk_array(list_currencies, 10)
     tasks = []
 
-    for symbol in list_currencies:
-        task = asyncio.create_task(connect(symbol))
+    for chunk in chunked_array:
+        task = asyncio.create_task(connect(chunk))
         tasks.append(task)
         await asyncio.sleep(1)
-        # print(check_activity, 'bababa')
     await asyncio.gather(*tasks)
 
 
