0e6a259a815c373249aec9b5a3c143930b121365
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-11-21 18:50:29 +0200

    Fixed AD218 ticket

--- lcx-fetcher.js
+++ lcx-fetcher.js
@@ -45,6 +45,10 @@ function getUnixTime(){
     return Math.floor(Date.now());
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 
 Number.prototype.noExponents = function() {
     var data = String(this).split(/[eE]/);
@@ -206,6 +210,10 @@ async function Connect(){
             }
         }catch(e){
             // skip confirmation messages cause they can`t be parsed into JSON format without an error
+            (async () => {
+                await sleep(1000); // Sleep for 1000 milliseconds (1 second) 
+                console.log(event.data);
+              })();
         }
         
         
@@ -227,6 +235,9 @@ async function Connect(){
     // func to handle errors
     ws.onerror = function(error) {
         console.log(`Error ${error} occurred`);
+        (async () => {
+            await sleep(1000); // Sleep for 1000 milliseconds (1 second) 
+          })();
     };
 }
 
