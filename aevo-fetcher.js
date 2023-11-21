8bb77b51b9fa6ae485b07f97d900b28a878f87f5
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-11-21 18:11:27 +0200

    Fixed AD218 ticket

--- aevo-fetcher.js
+++ aevo-fetcher.js
@@ -41,6 +41,10 @@ function getUnixTime(){
     return Math.floor(Date.now());
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 
 Number.prototype.noExponents = function() {
     var data = String(this).split(/[eE]/);
@@ -196,6 +200,10 @@ async function Connect(pair, index){
             }
         }catch(e){
             // skip confirmation messages cause they can`t be parsed into JSON format without an error
+            (async () => {
+                await sleep(1000); // Sleep for 1000 milliseconds (1 second) 
+                console.log(event.data);
+              })();
         }
         
         
@@ -217,6 +225,9 @@ async function Connect(pair, index){
     // func to handle errors
     ws.onerror = function(error) {
         console.log(`Error ${error} occurred`);
+        (async () => {
+            await sleep(1000); // Sleep for 1000 milliseconds (1 second) 
+          })();
     };
 }
 
