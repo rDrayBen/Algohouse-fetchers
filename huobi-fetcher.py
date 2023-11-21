eb549f7662a6e959ff3d551d4f94c7e437bead0c
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-11-21 17:25:16 +0200

    Fixed AD218 ticket

--- huobi-fetcher.py
+++ huobi-fetcher.py
@@ -161,9 +161,11 @@ async def main():
                     else:
                         print(dataJSON)
                 except Exception as e:
-                    print(f"Exception {e} occurred")
+                    print(f"Exception {e} occurred", data)
+                    time.sleep(1)
         except Exception as conn_e:
             print(f"WARNING: connection exception {conn_e} occurred")
+            time.sleep(1)
 
 
 # run main function
