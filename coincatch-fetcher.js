28117b9e8084723d4869d39d3fc8a53c4a98a673
RabotiahovDmytro <91627223+rDrayBen@users.noreply.github.com>
2023-11-21 18:36:24 +0200

    Fixed AD218 ticket

--- coincatch-fetcher.js
+++ coincatch-fetcher.js
@@ -31,6 +31,10 @@ function getUnixTime(){
     return Math.floor(Date.now());
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 
 Number.prototype.noExponents = function() {
     var data = String(this).split(/[eE]/);
@@ -300,6 +304,10 @@ async function Connect(pair){
             
         }catch(e){
             // skip confirmation messages cause they can`t be parsed into JSON format without an error
+            (async () => {
+                await sleep(1000); // Sleep for 1000 milliseconds (1 second) 
+                console.log(event.data);
+              })();
         }
         
         
@@ -321,6 +329,9 @@ async function Connect(pair){
     // func to handle errors
     ws.onerror = function(error) {
         console.log(`Error ${error} occurred`);
+        (async () => {
+            await sleep(1000); // Sleep for 1000 milliseconds (1 second) 
+          })();
     };
 }
 
