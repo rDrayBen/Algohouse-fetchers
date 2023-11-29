d49e18fd6f08a4315f3a0e9c166a30da4b245451
bohdandr <bohdandr12@gmail.com>
2023-11-30 00:40:17 +0200

    AD-248

--- fmfw-fetcher.py
+++ fmfw-fetcher.py
@@ -4,20 +4,45 @@ import websockets
 import time
 import asyncio
 import os
+import sys
 from CommonFunctions.CommonFunctions import get_unix_time, stats
 
+#default values
+MODE = "SPOT"
 currency_url = 'https://api.fmfw.io/api/2/public/symbol'
+WS_URL = 'wss://api.fmfw.io/api/2/ws/public'
+
+args = sys.argv[1:]
+if len(args) > 0:
+	for arg in args:
+		if arg.startswith('-') and arg[1:] == "perpetual":
+			MODE = "FUTURES"
+			# get all available symbol pairs
+			currency_url = 'https://api.fmfw.io/api/3/public/futures/info'
+			WS_URL = 'wss://st.fmfw.io/'
+			break
+else:
+	MODE = "SPOT"
+	# get all available symbol pairs
+	currency_url = 'https://api.fmfw.io/api/2/public/symbol'
+	WS_URL = 'wss://api.fmfw.io/api/2/ws/public'
+
 answer = requests.get(currency_url)
 currencies = answer.json()
 list_currencies = list()
-WS_URL = 'wss://api.fmfw.io/api/2/ws/public'
 is_subscribed_orderbooks = {}
 is_subscribed_trades = {}
 
-for element in currencies:
-	list_currencies.append(element["id"])
-	is_subscribed_trades[element["id"]] = False
-	is_subscribed_orderbooks[element["id"]] = False
+if MODE == "SPOT":
+	for element in currencies:
+		list_currencies.append(element["id"])
+		is_subscribed_trades[element["id"]] = False
+		is_subscribed_orderbooks[element["id"]] = False
+elif MODE == "FUTURES":
+	for key, value in currencies.items():
+		list_currencies.append(key.split("_")[0])
+		is_subscribed_trades[key.split("_")[0]] = False
+		is_subscribed_orderbooks[key.split("_")[0]] = False
 
 # for trades count stats
 symbol_trade_count_for_5_minutes = {}
@@ -32,44 +57,75 @@ for i in range(len(list_currencies)):
 
 # get metadata about each pair of symbols
 async def metadata():
-	for pair in currencies:
-		pair_data = '@MD ' + pair["baseCurrency"].upper() + pair["quoteCurrency"].upper() + ' spot ' + \
-					pair["baseCurrency"].upper() + ' ' + pair["quoteCurrency"].upper() + \
-					' ' + str(str(pair['tickSize'])[::-1].find('.')) + ' 1 1 0 0'
+	if MODE == "SPOT":
+		for pair in currencies:
+			pair_data = '@MD ' + pair["baseCurrency"].upper() + pair["quoteCurrency"].upper() + ' spot ' + \
+						pair["baseCurrency"].upper() + ' ' + pair["quoteCurrency"].upper() + \
+						' ' + str(str(pair['tickSize'])[::-1].find('.')) + ' 1 1 0 0'
 
-		print(pair_data, flush=True)
+			print(pair_data, flush=True)
+	elif MODE == "FUTURES":
+		for key, value in currencies.items():
+			pair_data = '@MD ' + key.split("_")[0] + ' perpetual ' + \
+						key.split("USDT")[0] + " USDT " + '-1 1 1 0 0'
+
+			print(pair_data, flush=True)
 
 	print('@MDEND')
 
 
 async def subscribe(ws):
 	while True:
-		for key, value in is_subscribed_trades.items():
+		if MODE == "SPOT":
+			for key, value in is_subscribed_trades.items():
 
-			if value == False:
+				if value == False:
 
-				# create the subscription for trades
-				await ws.send(json.dumps({
-					"method": "subscribeTrades",
-					"params": {
-						"symbol": f"{key}",
-						"limit": 100
-					},
-					"id": 123
-				}))
-
-				# resubscribe if orderbook subscription is not active + possibility to not subscribe or report orderbook changes:
-				if is_subscribed_orderbooks[key] == False and os.getenv("SKIP_ORDERBOOKS") == None:
-					# create the subscription for full orderbooks and updates
+					# create the subscription for trades
 					await ws.send(json.dumps({
-						"method": "subscribeOrderbook",
+						"method": "subscribeTrades",
 						"params": {
-							"symbol": f"{key}"
+							"symbol": f"{key}",
+							"limit": 100
 						},
 						"id": 123
 					}))
 
-					await asyncio.sleep(0.1)
+					# resubscribe if orderbook subscription is not active + possibility to not subscribe or report orderbook changes:
+					if is_subscribed_orderbooks[key] == False and os.getenv("SKIP_ORDERBOOKS") == None:
+						# create the subscription for full orderbooks and updates
+						await ws.send(json.dumps({
+							"method": "subscribeOrderbook",
+							"params": {
+								"symbol": f"{key}"
+							},
+							"id": 123
+						}))
+
+						await asyncio.sleep(0.1)
+
+		if MODE == "FUTURES":
+			for key, value in is_subscribed_trades.items():
+
+				if value == False:
+
+					# create the subscription for trades
+					await ws.send(json.dumps([
+						1,
+						"trades",
+						f"{key}_PERP"
+					]))
+
+					# resubscribe if orderbook subscription is not active + possibility to not subscribe or report orderbook changes:
+					if is_subscribed_orderbooks[key] == False and os.getenv("SKIP_ORDERBOOKS") == None:
+						# create the subscription for full orderbooks and updates
+						await ws.send(json.dumps([
+							1,
+							"orderbook",
+							f"{key}_PERP"
+						]))
+
+						await asyncio.sleep(0.1)
 
 		for el in list(is_subscribed_trades):
 			is_subscribed_trades[el] = False
@@ -81,37 +137,60 @@ async def subscribe(ws):
 
 def get_trades(var):
 	trade_data = var
-	if 'data' in trade_data["params"]:
-		for elem in trade_data["params"]["data"]:
-			print('!', get_unix_time(), trade_data["params"]['symbol'],
-				  "B" if elem["side"] == "buy" else "S", elem['price'],
-				  elem["quantity"], flush=True)
-			symbol_trade_count_for_5_minutes[trade_data["params"]['symbol']] += 1
-
+	if MODE == "SPOT":
+		if 'data' in trade_data["params"]:
+			for elem in trade_data["params"]["data"]:
+				print('!', get_unix_time(), trade_data["params"]['symbol'],
+				  	"B" if elem["side"] == "buy" else "S", elem['price'],
+				  	elem["quantity"], flush=True)
+				symbol_trade_count_for_5_minutes[trade_data["params"]['symbol']] += 1
+	elif MODE == "FUTURES":
+		if len(trade_data[3]) != 0 and len(trade_data[3]) < 3:
+			for elem in trade_data[3]:
+				print('!', get_unix_time(), trade_data[2].split("_")[0],
+				  	"B" if elem["side"] == "buy" else "S", str(elem['price']),
+				  	str(elem["quantity"]), flush=True)
+				symbol_trade_count_for_5_minutes[trade_data[2].split("_")[0]] += 1
 def get_order_books(var, update):
 	order_data = var
-	if 'ask' in order_data['params'] and len(order_data["params"]["ask"]) != 0:
-		symbol_orderbook_count_for_5_minutes[order_data['params']['symbol']] += len(order_data["params"]["ask"])
-		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['params']['symbol'] + ' S '
-		pq = "|".join(el["size"] + "@" + el["price"] for el in order_data["params"]["ask"])
-		answer = order_answer + pq
-		# checking if the input data is full orderbook or just update
-		if (update == True):
-			print(answer)
-		else:
+	if MODE == "SPOT":
+		if 'ask' in order_data['params'] and len(order_data["params"]["ask"]) != 0:
+			symbol_orderbook_count_for_5_minutes[order_data['params']['symbol']] += len(order_data["params"]["ask"])
+			order_answer = '$ ' + str(get_unix_time()) + " " + order_data['params']['symbol'] + ' S '
+			pq = "|".join(str(el["size"]) + "@" + str(el["price"]) for el in order_data["params"]["ask"])
+			answer = order_answer + pq
+			# checking if the input data is full orderbook or just update
+			if (update == True):
+				print(answer)
+			else:
+				print(answer + " R")
+
+		if 'bid' in order_data['params'] and len(order_data["params"]["bid"]) != 0:
+			symbol_orderbook_count_for_5_minutes[order_data['params']['symbol']] += len(order_data["params"]["bid"])
+			order_answer = '$ ' + str(get_unix_time()) + " " + order_data['params']['symbol'] + ' B '
+			pq = "|".join(str(el["size"]) + "@" + str(el["price"]) for el in order_data["params"]["bid"])
+			answer = order_answer + pq
+			# checking if the input data is full orderbook or just update
+			if (update == True):
+				print(answer)
+			else:
+				print(answer + " R")
+	elif MODE == "FUTURES":
+		if 'ask' in order_data[3] and len(order_data[3]["ask"]) != 0:
+			symbol_orderbook_count_for_5_minutes[order_data[2].split("_")[0]] += len(order_data[3]["ask"])
+			order_answer = '$ ' + str(get_unix_time()) + " " + order_data[2].split("_")[0] + ' S '
+			pq = "|".join(str(el[1]) + "@" + str(el[0]) for el in order_data[3]["ask"])
+			answer = order_answer + pq
+
 			print(answer + " R")
 
-	if 'bid' in order_data['params'] and len(order_data["params"]["bid"]) != 0:
-		symbol_orderbook_count_for_5_minutes[order_data['params']['symbol']] += len(order_data["params"]["bid"])
-		order_answer = '$ ' + str(get_unix_time()) + " " + order_data['params']['symbol'] + ' B '
-		pq = "|".join(el["size"] + "@" + el["price"] for el in order_data["params"]["bid"])
-		answer = order_answer + pq
-		# checking if the input data is full orderbook or just update
-		if (update == True):
-			print(answer)
-		else:
-			print(answer + " R")
+		if 'bid' in order_data[3] and len(order_data[3]["bid"]) != 0:
+			symbol_orderbook_count_for_5_minutes[order_data[2].split("_")[0]] += len(order_data[3]["bid"])
+			order_answer = '$ ' + str(get_unix_time()) + " " + order_data[2].split("_")[0] + ' B '
+			pq = "|".join(str(el[1]) + "@" + str(el[0]) for el in order_data[3]["bid"])
+			answer = order_answer + pq
 
+			print(answer + " R")
 
 async def heartbeat(ws):
 	while True:
@@ -145,39 +224,62 @@ async def main():
 			# create task to subscribe to symbols` pair
 			subscription = asyncio.create_task(subscribe(ws))
 
-			# create task to keep connection alive
-			pong = asyncio.create_task(heartbeat(ws))
+			if MODE == "SPOT":
+				# create task to keep connection alive
+				pong = asyncio.create_task(heartbeat(ws))
+			elif MODE == "FUTURES":
+				pass
 
 			while True:
 				data = await ws.recv()
 
-				dataJSON = json.loads(data)
+				if data != "pong":
+					dataJSON = json.loads(data)
+					if MODE == "SPOT" and "method" in dataJSON:
+						try:
+							# if received data is about trades
+							if dataJSON['method'] == 'updateTrades':
+								is_subscribed_trades[dataJSON['params']["symbol"]] = True
+								get_trades(dataJSON)
 
-				if "method" in dataJSON:
+							# if received data is about updates
+							if dataJSON['method'] == 'updateOrderbook':
+								is_subscribed_orderbooks[dataJSON['params']["symbol"]] = True
+								get_order_books(dataJSON, update=True)
 
-					try:
+							# if received data is about orderbooks
+							if dataJSON['method'] == 'snapshotOrderbook':
+								is_subscribed_orderbooks[dataJSON['params']["symbol"]] = True
+								get_order_books(dataJSON, update=False)
 
-						# if received data is about trades
-						if dataJSON['method'] == 'updateTrades':
-							is_subscribed_trades[dataJSON['params']["symbol"]] = True
-							get_trades(dataJSON)
+							else:
+								pass
 
-						# if received data is about updates
-						if dataJSON['method'] == 'updateOrderbook':
-							is_subscribed_orderbooks[dataJSON['params']["symbol"]] = True
-							get_order_books(dataJSON, update=True)
+						except Exception as ex:
+							print(f"Exception {ex} occurred", data)
+							time.sleep(1)
 
-						# if received data is about orderbooks
-						if dataJSON['method'] == 'snapshotOrderbook':
-							is_subscribed_orderbooks[dataJSON['params']["symbol"]] = True
-							get_order_books(dataJSON, update=False)
+					elif MODE == "FUTURES":
+						try:
+							# if received data is about trades
+							if dataJSON[1] == 'trades':
+								is_subscribed_trades[dataJSON[2].split("_")[0]] = True
+								get_trades(dataJSON)
 
-						else:
-							pass
+							# if received data is about orderbooks
+							if dataJSON[1] == 'orderbook':
+								is_subscribed_orderbooks[dataJSON[2].split("_")[0]] = True
+								get_order_books(dataJSON, update=False)
 
-					except Exception as ex:
-						print(f"Exception {ex} occurred", data)
-						time.sleep(1)
+							else:
+								pass
+
+						except Exception as ex:
+							print(f"Exception {ex} occurred", data)
+							time.sleep(1)
+					else:
+						pass
+
 
 		except Exception as conn_ex:
 			print(f"Connection exception {conn_ex} occurred")
